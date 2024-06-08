22197f8d01ee61fc93cf0890e68784b197e65070
Rishabh Raj <rishabhraj1572@gmail.com>
2024-06-08 19:55:26 +0530

    updated login and signup activities

---
+++ app/src/main/java/com/gemx/gemx/Watchers/OtpTextWatcher.java
@@ -0,0 +1,36 @@
+package com.gemx.gemx.Watchers;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+public class OtpTextWatcher implements TextWatcher {
+    private final EditText currentEditText;
+    private final EditText nextEditText;
+    private final EditText prevEditText;
+
+    public OtpTextWatcher(EditText currentEditText, EditText nextEditText, EditText prevEditText) {
+        this.currentEditText = currentEditText;
+        this.nextEditText = nextEditText;
+        this.prevEditText = prevEditText;
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        //no need
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        //no need
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        if (s.length() == 1 && nextEditText != null) {
+            nextEditText.requestFocus();
+        } else if (s.length() == 0 && prevEditText != null) {
+            prevEditText.requestFocus();
+        }
+    }
+}
