c6eff51f39f72414c86b86f42b1b77dc81e729e2
Rishabh Raj <rishabhraj1572@gmail.com>
2024-06-10 15:07:23 +0530

    activity_login line 136

--- app/src/main/java/com/gemx/gemx/LoginActivity.java
+++ app/src/main/java/com/gemx/gemx/LoginActivity.java
@@ -1,12 +1,7 @@
 package com.gemx.gemx;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
