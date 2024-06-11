e50ce029acf587dc474966aab874068ea95ea394
Sunatqayoom <115891537+Sunatqayoom@users.noreply.github.com>
2024-06-11 20:02:53 +0530

    message

--- app/src/main/java/com/gemx/gemx/MainActivity.java
+++ app/src/main/java/com/gemx/gemx/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    //edfetogedwe
+    //edfetogedweferwe
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
