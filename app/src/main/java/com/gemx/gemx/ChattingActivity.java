efdf1df46720884b9701f081ea4e632bee0e220a
Sunatqayoom <115891537+Sunatqayoom@users.noreply.github.com>
2024-06-12 13:18:47 +0530

    page 19

--- app/src/main/java/com/gemx/gemx/ChattingActivity.java
+++ app/src/main/java/com/gemx/gemx/ChattingActivity.java
@@ -1,7 +1,11 @@
 package com.gemx.gemx;
 
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +21,17 @@ public class ChattingActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_chatting);
         ImageView backBtn = findViewById(R.id.back);
+        TextView gradientTextView = findViewById(R.id.anything);
+
+        Shader textShader = new LinearGradient(0, 0, 0, gradientTextView.getTextSize(),
+                new int[]{
+                        Color.parseColor("#3A59C7"),
+                        Color.parseColor("#FFFFFF")
+                }, null, Shader.TileMode.CLAMP);
+
+        gradientTextView.getPaint().setShader(textShader);
+        gradientTextView.invalidate();
+
 
         backBtn.setOnClickListener(v-> finish());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
