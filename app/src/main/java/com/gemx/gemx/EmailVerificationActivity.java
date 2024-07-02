99a67ac0f2c0f95271e06c08942a029527faac48
Rishabh Raj <rishabhraj1572@gmail.com>
2024-07-02 23:52:33 +0530

    hot fix : login and signup verification

--- app/src/main/java/com/gemx/gemx/EmailVerificationActivity.java
+++ app/src/main/java/com/gemx/gemx/EmailVerificationActivity.java
@@ -11,6 +11,7 @@ import android.text.SpannableStringBuilder;
 import android.text.Spanned;
 import android.text.style.ForegroundColorSpan;
 import android.text.style.StyleSpan;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -21,10 +22,21 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class EmailVerificationActivity extends AppCompatActivity {
 
@@ -32,6 +44,8 @@ public class EmailVerificationActivity extends AppCompatActivity {
     FirebaseAuth mAuth;
     ProgressDialog progressDialog;
     private TextView emailVerificationSt;
+    private GoogleSignInClient mGoogleSignInClient;
+    private static final int RC_SIGN_IN = 9001;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -93,6 +107,31 @@ public class EmailVerificationActivity extends AppCompatActivity {
             }
         }
 
+        //glogin
+        ImageView gLogin = findViewById(R.id.gmail);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        gLogin.setOnClickListener(v->{
+            progressDialog.setTitle("Please Wait");
+            progressDialog.setMessage("Logging In...");
+            progressDialog.show();
+            signIn();
+        });
+
+
+        //signin button
+        TextView signinBtn = findViewById(R.id.signin);
+        signinBtn.setOnClickListener(v-> {
+            startActivity(new Intent(this, LoginActivity.class));
+            finishAffinity();
+        });
+
     }
 
     private void verifyEmail() {
@@ -145,6 +184,59 @@ public class EmailVerificationActivity extends AppCompatActivity {
             progressDialog.dismiss();
             Toast.makeText(this, "Email verification failed", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                progressDialog.dismiss();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("glogin", "signInWithCredential:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        Map<String, Object> userData = new HashMap<>();
+                        userData.put("userID", user.getUid());
+                        userData.put("email_phone", user.getEmail());
+                        userData.put("name",user.getDisplayName());
+
+                        // Save the user data to Firestore
+                        FirebaseFirestore db = FirebaseFirestore.getInstance();
+                        db.collection("users").document(user.getUid())
+                                .set(userData)
+                                .addOnSuccessListener(aVoid -> {
+                                    Log.d("Save Status","User Details Saved");
+                                    Intent i = new Intent(EmailVerificationActivity.this, StartConversationActivity.class);
+                                    startActivity(i);
+                                    finishAffinity();
+                                })
+                                .addOnFailureListener(e -> {
+                                    Log.d("Save Status","User Details Not Saved");
+                                });
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("glogin", "signInWithCredential:failure", task.getException());
+                        progressDialog.dismiss();
+                    }
+                });
     }
 
     @Override
